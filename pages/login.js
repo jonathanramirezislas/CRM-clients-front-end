85cb4a89c092035bc010cd22d1f1158caf3afc4f
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-05-11 15:27:41 -0500

    protect routes

---
+++ pages/login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Login = () => {
+    return (
+        <Layout>
+            <h1 className="text-center text-2xl text-white font-light"></h1>
+            <div className="flex justify-center mt-5">
+                <div className="w-full max-w-sm">
+            
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Login
---
+++ pages/nuevacuenta.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nuevacuenta = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Nuevacuenta
