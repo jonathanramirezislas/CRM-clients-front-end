99f6edbb2935ad9bf5dd4475f1b88627d750cc89
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-05-12 15:58:00 -0500

    set up apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import 'tailwindcss/tailwind.css'
+import { ApolloProvider } from "@apollo/client";
+import "tailwindcss/tailwind.css";
+import client from '../config/apollo'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
